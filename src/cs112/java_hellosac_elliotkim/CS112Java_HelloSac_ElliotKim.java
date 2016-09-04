8d393d8274075689275f4e6c2572f01ef83ce361
User <User@Elliot_Laptop02>
2016-09-03 20:45:43 -0700

    new commit

---
+++ src/cs112/java_hellosac_elliotkim/CS112Java_HelloSac_ElliotKim.java
@@ -0,0 +1,17 @@
+/*
+ * Subject:     CS112-Java Homework Week02 -Upload HelloSac app to GITHUB
+ * Due:         Sunday, 9/4/2016
+ * Programmer:  Elliot Kim
+ */
+package cs112.java_hellosac_elliotkim;
+
+
+public class CS112Java_HelloSac_ElliotKim 
+{
+
+    public static void main(String[] args) 
+    {
+    System.out.println("Hello Sac!");
+    }
+    
+}
